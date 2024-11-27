225c73176ae7a6c07358829c596ead356d431c99
Sihle <sihlesithole810@gmail.com>
2024-11-27 12:55:45 +0200

    PortfolioUpdated database

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text" ,{
+	strings: ["Frontend Developer" , "Backend Developer" , "Scrum Master" , "Database administrator and development"],
+	typeSpeed:100,
+	backSpeed:100,
+	backDelay:1000,
+	loop: true
+})
